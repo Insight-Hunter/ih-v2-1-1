98dccb882e44568f2b892c5a788999146ca91a56
James Michael Hunter Turner <michael@turneratlanta.com>
2025-09-06 17:22:57 -0400

    Create App.tsx
    Signed-off-by: James Michael Hunter Turner <michael@turneratlanta.com>

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>Welcome to Insight Hunter</h1>
+      <p>Starting development on Cloudflare Workers with React + Vite + TypeScript.</p>
+    </div>
+  );
+};
+
+export default App;
