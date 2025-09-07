131c53b17698c94a06dcd7082ae00b6d2a56125a
James Michael Hunter Turner <michael@turneratlanta.com>
2025-09-07 14:08:25 -0400

    Update App.tsx
    Signed-off-by: James Michael Hunter Turner <michael@turneratlanta.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import Onboarding from './onboarding/Onboarding';
 
-const App: React.FC = () => <Onboarding />;
-
 const App: React.FC = () => {
-  return (
-    <div className="app-container">
-      <h1>Welcome to Insight Hunter</h1>
-      <p>Starting development on Cloudflare Workers with React + Vite + TypeScript.</p>
-    </div>
-  );
+  return <Onboarding />;
 };
 
-export default 
+export default App;
